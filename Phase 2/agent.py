c17b4a7d706849d6d327f2c610a8832d28a73c0b
Evan Grau <ekg20b@acu.edu>
2024-05-03 13:36:08 -0500

    test

--- Phase 2/agent.py
+++ Phase 2/agent.py
@@ -5,45 +5,45 @@ PERCENTAGE = 1.0 # global learning decay variable
 
 # decide if pieces are flippable in this direction
 def flips( board, index, piece, step ):
-   other = ('X' if piece == 'O' else 'O')
-   # is an opponent's piece in first spot that way?
-   here = index + step
-   if here < 0 or here >= 36 or board[here] != other:
-      return False
-      
-   if( abs(step) == 1 ): # moving left or right along row
-      while( here // 6 == index // 6 and board[here] == other ):
-         here = here + step
-      # are we still on the same row and did we find a matching endpiece?
-      return( here // 6 == index // 6 and board[here] == piece )
-   
-   else: # moving up or down (possibly with left/right tilt)
-      while( here >= 0 and here < 36 and board[here] == other ):
-         here = here + step
-      # are we still on the board and did we find a matching endpiece?
-      return( here >= 0 and here < 36 and board[here] == piece )
-   
-# decide if this is a valid move
-def isValidMove( b, x, p ): # board, index, piece
-   # invalid index
-   if x < 0 or x >= 36:
-      return False
-   # space already occupied
-   if b[x] != '-':
-      return False 
-   # otherwise, check for flipping pieces
-   up    = x >= 12   # at least third row down
-   down  = x <  24   # at least third row up
-   left  = x % 6 > 1 # at least third column
-   right = x % 6 < 4 # not past fourth column
-   return (          left  and flips(b,x,p,-1)  # left
-         or up   and left  and flips(b,x,p,-7)  # up/left
-         or up             and flips(b,x,p,-6)  # up
-         or up   and right and flips(b,x,p,-5)  # up/right
-         or          right and flips(b,x,p, 1)  # right
-         or down and right and flips(b,x,p, 7)  #down/right
-         or down           and flips(b,x,p, 6)  # down
-         or down and left  and flips(b,x,p, 5)) # down/left
+    other = ('X' if piece == 'O' else 'O')
+    # is an opponent's piece in first spot that way?
+    here = index + step
+    if here < 0 or here >= 36 or board[here] != other:
+        return False
+        
+    if( abs(step) == 1 ): # moving left or right along row
+        while( here // 6 == index // 6 and board[here] == other ):
+            here = here + step
+        # are we still on the same row and did we find a matching endpiece?
+        return( here // 6 == index // 6 and board[here] == piece )
+
+    else: # moving up or down (possibly with left/right tilt)
+        while( here >= 0 and here < 36 and board[here] == other ):
+            here = here + step
+        # are we still on the board and did we find a matching endpiece?
+        return( here >= 0 and here < 36 and board[here] == piece )
+
+    # decide if this is a valid move
+    def isValidMove( b, x, p ): # board, index, piece
+    # invalid index
+    if x < 0 or x >= 36:
+        return False
+    # space already occupied
+    if b[x] != '-':
+        return False 
+    # otherwise, check for flipping pieces
+    up    = x >= 12   # at least third row down
+    down  = x <  24   # at least third row up
+    left  = x % 6 > 1 # at least third column
+    right = x % 6 < 4 # not past fourth column
+    return (          left  and flips(b,x,p,-1)  # left
+            or up   and left  and flips(b,x,p,-7)  # up/left
+            or up             and flips(b,x,p,-6)  # up
+            or up   and right and flips(b,x,p,-5)  # up/right
+            or          right and flips(b,x,p, 1)  # right
+            or down and right and flips(b,x,p, 7)  #down/right
+            or down           and flips(b,x,p, 6)  # down
+            or down and left  and flips(b,x,p, 5)) # down/left
 
 class Agent:
 
@@ -142,6 +142,7 @@ class Agent:
 
             if status == 1:
                 # agent won, add percentage/i to that prob and subtract the appropriate amount from the rest
+                # test push for git webhook
                 mult = PERCENTAGE / (i + 1)
                 prob = 0
                 tile = 0
